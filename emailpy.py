e4bc82c08b834455b58e05ad6f4f426084449cc4
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-10-28 18:46:58 -0400

    send quick email no body

---
+++ emailpy.py
@@ -0,0 +1,8 @@
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("abc@gmail.com", "slickrick2016B")
+msg = "Just wrote my first little code for sending emails directly from python"
+server.sendmail("abc@gmail.com", "persongetting@gmail.com", msg)
+server.quit()
