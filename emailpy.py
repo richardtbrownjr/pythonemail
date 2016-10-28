d0129c53f9fc20ae686407b1121cef432e0bf755
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2016-10-28 18:50:02 -0400

    update email password

--- emailpy.py
+++ emailpy.py
@@ -2,7 +2,7 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("abc@gmail.com", "slickrick2016B")
+server.login("abc@gmail.com", "mythoughts&ideas")
 msg = "Just wrote my first little code for sending emails directly from python"
 server.sendmail("abc@gmail.com", "persongetting@gmail.com", msg)
 server.quit()
